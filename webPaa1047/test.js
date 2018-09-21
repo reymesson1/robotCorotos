08ccfa4a3bfee5d822da47b39bb7f503aaa76557
reymesson1 <reymesson1>
2018-09-21 01:03:10 -0400

    robot update

--- webPaa1047/test.js
+++ webPaa1047/test.js
@@ -1,4 +1,10 @@
+var mongoose = require('mongoose');
 var webdriver = require('selenium-webdriver');
+var express = require('express');
+var app = express();
+
+var Post = require('./models/post.js');
+
 var By = webdriver.By;
 
 var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
@@ -12,10 +18,25 @@ driver.findElement(By.className('_2QhqT _3NHOV _3z-oQ _2El2O _7xPBo _2pXQq _3WoT
 
 driver.wait(webdriver.until.elementLocated(webdriver.By.id('otp-verification-code')),20000)
 .then(()=>{
-   return driver.findElement(webdriver.By.id('otp-verification-code')).sendKeys('123456');
+  setTimeout(async() => {
+    console.log('esperar....')
+    var posts = await Post.find({})
+    console.log(posts[0].code);
+    return driver.findElement(webdriver.By.id('otp-verification-code')).sendKeys(posts[0].code);
+  }, 15000);
+  
 }).then((element)=>{
-    // Perform any operation what you to do.
+    setTimeout(async() => {      
+      console.log('esperar2....')
       return driver.findElement(By.className('_2QhqT _3NHOV _3z-oQ _2El2O _7xPBo _2pXQq _3WoTQ gO5qu _2h31L _1Dh7N')).click()
+      var posts = Post.remove({})
+    }, 20000);
 }).catch(()=>{
-     console.log('Element not found');
+    console.log('Element not found');
+})
+
+mongoose.connect('mongodb://localhost:27017/meanstack',(err)=>{
+  if(!err){
+      console.log('Connected to mongo Database');
+  }
 })
